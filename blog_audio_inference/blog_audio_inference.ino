a78037c0c2739a21588de46f56f177944d48aafe
Daniele P <d.pitotti@outlook.it>
2024-09-09 17:39:58 +0200

    first commit

---
+++ blog_audio_inference/blog_audio_inference.ino
@@ -0,0 +1,142 @@
+#include <Blog-Project_inferencing.h>
+
+// Numero di campioni da leggere dal pin analogico
+#define NUM_SAMPLES 400
+
+// Array per memorizzare i campioni del segnale analogico
+float features[NUM_SAMPLES] = {0};
+
+/**
+ * @brief      Copy raw feature data in out_ptr
+ *             Function called by inference library
+ *
+ * @param[in]  offset   The offset
+ * @param[in]  length   The length
+ * @param      out_ptr  The out pointer
+ *
+ * @return     0
+ */
+int raw_feature_get_data(size_t offset, size_t length, float *out_ptr) {
+    memcpy(out_ptr, features + offset, length * sizeof(float));
+    return 0;
+}
+
+void print_inference_result(ei_impulse_result_t result);
+
+/**
+ * @brief      Arduino setup function
+ */
+void setup()
+{
+    // put your setup code here, to run once:
+    Serial.begin(115200);
+    // comment out the below line to cancel the wait for USB connection (needed for native USB)
+    while (!Serial);
+    Serial.println("Edge Impulse Inferencing Demo");
+
+    // Imposta il pin analogico
+    analogReadResolution(12); // Risoluzione a 12 bit per il Raspberry Pi Pico
+}
+
+/**
+ * @brief      Arduino main function
+ */
+void loop()
+{
+    ei_printf("Edge Impulse standalone inferencing (Arduino)\n");
+
+    // Leggi i campioni dal pin analogico GP26 (A0)
+    for (int i = 0; i < NUM_SAMPLES; i++) {
+        features[i] = analogRead(A0); // GP26 è mappato come A0
+        delay(10); // Ritardo per la lettura del prossimo campione
+    }
+
+    if (NUM_SAMPLES != EI_CLASSIFIER_DSP_INPUT_FRAME_SIZE) {
+        ei_printf("The size of your 'features' array is not correct. Expected %lu items, but had %d\n",
+            EI_CLASSIFIER_DSP_INPUT_FRAME_SIZE, NUM_SAMPLES);
+        delay(1000);
+        return;
+    }
+
+    ei_impulse_result_t result = { 0 };
+
+    // the features are stored into flash, and we don't want to load everything into RAM
+    signal_t features_signal;
+    features_signal.total_length = NUM_SAMPLES;
+    features_signal.get_data = &raw_feature_get_data;
+
+    // invoke the impulse
+    EI_IMPULSE_ERROR res = run_classifier(&features_signal, &result, false /* debug */);
+    if (res != EI_IMPULSE_OK) {
+        ei_printf("ERR: Failed to run classifier (%d)\n", res);
+        return;
+    }
+
+    // print inference return code
+    ei_printf("run_classifier returned: %d\r\n", res);
+    print_inference_result(result);
+
+    delay(1000);
+}
+
+void print_inference_result(ei_impulse_result_t result) {
+    // Print how long it took to perform inference
+    ei_printf("Timing: DSP %d ms, inference %d ms, anomaly %d ms\r\n",
+            result.timing.dsp,
+            result.timing.classification,
+            result.timing.anomaly);
+
+    // Print the prediction results (object detection)
+#if EI_CLASSIFIER_OBJECT_DETECTION == 1
+    ei_printf("Object detection bounding boxes:\r\n");
+    for (uint32_t i = 0; i < result.bounding_boxes_count; i++) {
+        ei_impulse_result_bounding_box_t bb = result.bounding_boxes[i];
+        if (bb.value == 0) {
+            continue;
+        }
+        ei_printf("  %s (%f) [ x: %u, y: %u, width: %u, height: %u ]\r\n",
+                bb.label,
+                bb.value,
+                bb.x,
+                bb.y,
+                bb.width,
+                bb.height);
+    }
+
+    // Print the prediction results (classification)
+#else
+    ei_printf("Predictions:\r\n");
+    for (uint16_t i = 0; i < EI_CLASSIFIER_LABEL_COUNT; i++) {
+        ei_printf("  %s: ", ei_classifier_inferencing_categories[i]);
+        ei_printf("%.5f\r\n", result.classification[i].value);
+        if(result.classification[i].value > 0.60) {
+          Serial.print("rilevato: "); Serial.println(ei_classifier_inferencing_categories[i]);
+        }
+    }
+#endif
+
+    // Print anomaly result (if it exists)
+#if EI_CLASSIFIER_HAS_ANOMALY
+    ei_printf("Anomaly prediction: %.3f\r\n", result.anomaly);
+#endif
+
+#if EI_CLASSIFIER_HAS_VISUAL_ANOMALY
+    ei_printf("Visual anomalies:\r\n");
+    for (uint32_t i = 0; i < result.visual_ad_count; i++) {
+        ei_impulse_result_bounding_box_t bb = result.visual_ad_grid_cells[i];
+        if (bb.value == 0) {
+            continue;
+        }
+        ei_printf("  %s (%f) [ x: %u, y: %u, width: %u, height: %u ]\r\n",
+                bb.label,
+                bb.value,
+                bb.x,
+                bb.y,
+                bb.width,
+                bb.height);
+    }
+#endif
+}
+
+
+
